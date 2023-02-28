7c40025c352d2f1acfe4c76d9d95f8f094675a7c
Miles <milesrhowens@gmail.com>
2023-02-28 14:41:09 -0700

    Finished responsiveness except git link

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -89,19 +89,19 @@ $(document).ready(function(){
     });
   });
 
-  /**
-   * Glow-effect on cards
-   */
-  document.getElementById("projects-cards").onmousemove = (e) => {
-    for (const card of document.getElementsByClassName("card")) {
-      const rect = card.getBoundingClientRect(),
-        x = e.clientX - rect.left,
-        y = e.clientY - rect.top;
+  // /**
+  //  * Glow-effect on cards
+  //  */
+  // document.getElementById("projects-cards").onmousemove = (e) => {
+  //   for (const card of document.getElementsByClassName("card")) {
+  //     const rect = card.getBoundingClientRect(),
+  //       x = e.clientX - rect.left,
+  //       y = e.clientY - rect.top;
 
-      card.style.setProperty("--mouse-x", `${x}px`);
-      card.style.setProperty("--mouse-y", `${y}px`);
-    }
-  }
+  //     card.style.setProperty("--mouse-x", `${x}px`);
+  //     card.style.setProperty("--mouse-y", `${y}px`);
+  //   }
+  // }
 
   /**
    * 
